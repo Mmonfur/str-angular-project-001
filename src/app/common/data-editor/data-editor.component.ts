dd1c677ff7ba1bcd40b2094a63f95c839ca18b76
cherryApp <j.cserko@gmail.com>
2021-02-07 20:11:55 +0100

    Feat: data-editor component, admin.component.

--- src/app/common/data-editor/data-editor.component.ts
+++ src/app/common/data-editor/data-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from 'src/app/model/product';
 import { ConfigService, ITableCol } from 'src/app/service/config.service';
 import { ProductService } from 'src/app/service/product.service';
@@ -10,42 +11,50 @@ import { ProductService } from 'src/app/service/product.service';
   styleUrls: ['./data-editor.component.scss']
 })
 export class DataEditorComponent implements OnInit {
-  
+
   @Input() product: Product[] = [];
   @Input() icon: string = 'fa-pen';
   @Input() btnClass: string = 'btn-info';
   @Input() text: string = '';
 
-  
-  ProductService: any;
-  
+  /**
+   * @var cols {ITableCol[]} - a táblázat oszlopai a ConfigService -ből.
+   */
+  cols: ITableCol[] = this.config.tableCols;
 
-  constructor() { 
-    
+  /**
+   * @var productList$ {Observable<Product[]>} - a termékeket szolgáltató Observable.
+   */
+  productList$: Observable<Product[]> = this.productService.getAll();
+
+
+  /**
+   *
+   * @param productService {ProductService} - a termékek szolgáltatása
+   * @param config {ConfigService} - a beállítások szolgáltatása
+   */
+  constructor(
+    private productService: ProductService,
+    private config: ConfigService,
+  ) {
+
   }
 
   ngOnInit(): void {
   }
 
-  onUpdate(product:Product): void {
-    this.ProductService.update(product).subscribe(
+  onUpdate(product: Product): void {
+    this.productService.update(product).subscribe(
       updatedProduct => console.log(updatedProduct)
-      
+
     );
   }
 
-  onDelete(product:Product): void {
-    this.ProductService.remove(product).subscribe(
+  onDelete(product: Product): void {
+    this.productService.remove(product).subscribe(
       () => console.log('deleted')
     );
   }
-  
-  onSelectProduct(product: Product): void {
-    this.ProductService.select(product).subscribe(
-      () => console.log('selected')
-    );
-  }
-
 
 }
 
