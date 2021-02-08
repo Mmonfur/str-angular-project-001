d0531715523f36ccc5ab9472db0b6041fccca536
ormiati <attila.ormos@gmail.com>
2021-02-08 23:17:03 +0100

    category 2 szűrés

--- src/app/page/cat02/cat02.component.ts
+++ src/app/page/cat02/cat02.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Base } from 'src/app/controller/base';
+import { Product } from 'src/app/model/product';
+import { ConfigService, IHeadline } from 'src/app/service/config.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-cat02',
   templateUrl: './cat02.component.html',
   styleUrls: ['./cat02.component.scss']
 })
-export class Cat02Component implements OnInit {
+export class Cat02Component extends Base implements OnInit {
 
-  constructor() { }
+  lines: IHeadline[] = this.config.headlines;
+  productList$: Observable<Product[]> = this.productService.getAll().pipe(
+    map(products => products.filter(product=> product.catId=1001))
+  );
+
+  constructor(
+    public productService: ProductService,
+    private config: ConfigService,
+    ) {
+      super(productService);
+    }
 
   ngOnInit(): void {
   }
