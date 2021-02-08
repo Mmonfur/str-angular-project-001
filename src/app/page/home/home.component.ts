85cd5dc25f7ae9ec1e8ed1fabd83a1cf546167a8
ormiati <attila.ormos@gmail.com>
2021-02-08 23:20:24 +0100

    home módosítás

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -10,10 +10,7 @@ import { ProductService } from 'src/app/service/product.service';
 })
 export class HomeComponent extends Base implements OnInit {
 
-  featuredProducts: Product[] = this.productService.getFeatured(true)
-    .slice(0, 5);
-    allProducts: Product[] = this.productService.list;
-  
+
   constructor(
     public productService: ProductService,
   ) {
